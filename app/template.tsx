d7405438735333f71e784002137440abf165a3ee
Simreich Somogod <simreich1989@gmail.com>
2025-01-05 10:29:59 +0800

    update: enhance animation with AnimatePresence for smoother transitions

--- app/template.tsx
+++ app/template.tsx
@@ -1,17 +1,20 @@
 "use client"
 
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.div
-        // initial={{ opacity: 0}}
-        // animate={{ opacity: 1}}
-        // transition={{ duration: 0.75, ease: 'easeInOut'}}
-        initial={{ opacity: 0, x: -100}}
-        animate={{ opacity: 1, x: 0}}
-        exit={{ opacity: 0, x: 100}}
-        transition={{ type: 'spring', duration: 0.75, bounce: 0.25}}
-    >{children}</motion.div>
-  )
+    return (
+        <AnimatePresence mode="wait">
+            <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 100 }}
+                transition={{ type: "spring", duration: 1.5, bounce: 0.25 }}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    )
 }
+
+
